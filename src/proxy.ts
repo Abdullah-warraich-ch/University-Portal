86ebc6c9f193dcb552ad463543936388e08edff6
Abdullah-warraich-ch <abdullahnasar333@gmail.com>
2026-01-28 20:56:40 +0500

    Login Done

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,14 +2,15 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const response = NextResponse.next();
   const role = request.cookies.get("role")?.value;
   const token = request.cookies.get("token")?.value;
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
-  return response;
+  console.log(pathname);
+  return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
